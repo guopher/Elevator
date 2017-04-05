99875866a93aee2ec2d949473ac8a8541d27b16a
jjguo <jjguo@jjs-macbook-pro.home>
2017-04-04 20:14:03 -0700

    Add a simple output change

--- First Github/src/Client.java
+++ First Github/src/Client.java
@@ -2,5 +2,6 @@ public class Client {
 	public static void main(String[] args) {
 		Elevator mcmahon = new Elevator(3);
 		System.out.println("Currently level " + mcmahon.getLevel());
+		System.out.println("Test 1");
 	}
 }
