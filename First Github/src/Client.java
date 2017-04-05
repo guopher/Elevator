5499b559eac8591dc7ef41d322a16b9055bd00f9
JJ Guo <jjguo123@gmail.com>
2017-04-04 20:01:58 -0700

    Delete first line

---
+++ First Github/src/Client.java
@@ -0,0 +1,6 @@
+public class Client {
+	public static void main(String[] args) {
+		Elevator mcmahon = new Elevator(3);
+		System.out.println("Currently level " + mcmahon.getLevel());
+	}
+}
---
+++ First Github/src/Elevator.java
@@ -0,0 +1,11 @@
+public class Elevator {
+	private int level;
+	
+	public Elevator(int level) {
+		this.level = level;
+	}
+	
+	public int getLevel() {
+		return this.level;
+	}
+}
